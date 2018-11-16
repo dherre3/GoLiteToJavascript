fb74a8b176640d6396c059105826ea7e2debf3f6
David Herrera <davidfherrerar@gmail.com>
2018-11-16 13:31:15 -0500

    Change of quotation style for efficiency

--- src/app/compiler-request.service.ts
+++ src/app/compiler-request.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
-import {CustomEncoder} from "./custom-encoder.class";
+import {CustomEncoder} from './custom-encoder.class';
 @Injectable()
 export class CompilerRequestService {
   private programSubject = new Subject<any>();
